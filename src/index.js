5ff2eee3c84925b50818e672701eb004b8c7f1cf
Iwan Rahman <1w3n.rahman@gmail.com>
2023-12-25 23:52:08 -0500

    Parse weather data from WeatherAPI

--- src/index.js
+++ src/index.js
@@ -8,7 +8,29 @@ const weatherImg = document.querySelector("img");
 async function getWeather(location = 'toronto'){
   const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=4db0f8caba1f4ca3991200732232512&q='${location}`);
   weatherImg.alt = location;
-  console.log(response);
+  const weatherData = await response.json();
+  const weatherCondition = weatherData.current.condition.text;
+  weatherImg.alt = `${weatherCondition} image`;
+  console.log(weatherCondition);
+  switch(weatherCondition){
+    case 'Clear':
+      weatherImg.src = '';
+      break;
+    case 'Overcast':
+      weatherImg.src = '';
+      break;
+    case 'Rain':
+      weatherImg.src = '';
+      break;
+    case 'Snow':
+      weatherImg.src = '';
+      break;
+    case 'Storm':
+      weatherImg.src = '';
+      break;
+    default:
+      weatherImg.src = weatherData.current.condition.icon;
+  }
 }
 
 getWeather();
