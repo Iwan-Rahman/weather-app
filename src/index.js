c5f126ff0a9fc0727e7daef088888f515875fb51
Iwan Rahman <1w3n.rahman@gmail.com>
2023-12-25 23:23:46 -0500

    Fetch Location Information from WeatherAPI
    Create fetch request to WeatherAPI
    Setup repo from template repo
    Add minor html and css styling

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ import './style.css';
 
 const title = document.querySelector("h1");
 title.classList.add('hello');
+
+const weatherImg = document.querySelector("img");
+
+async function getWeather(location = 'toronto'){
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=4db0f8caba1f4ca3991200732232512&q='${location}`);
+  weatherImg.alt = location;
+  console.log(response);
+}
+
+getWeather();
