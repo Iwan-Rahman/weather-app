7150cfc5668a2d3a449784f0bfc15c8e5400e078
Iwan Rahman <1w3n.rahman@gmail.com>
2023-12-26 00:21:04 -0500

    Style weather display
    Fetch weather images from giphy
    Display weather images depending on weather condition
    Style page

--- src/index.js
+++ src/index.js
@@ -4,29 +4,45 @@ const title = document.querySelector("h1");
 title.classList.add('hello');
 
 const weatherImg = document.querySelector("img");
+const weatherLabel = document.querySelector(".main h2");
+const locationLabel = document.querySelector(".main h3");
 
-async function getWeather(location = 'toronto'){
+const weatherImages = {
+  'sunny': await fetch('https://media.giphy.com/media/UnyblOs6hGx9Mli7jq/giphy.gif'),
+  'night': await fetch('https://media.giphy.com/media/MZXCIvPneFNeFF18Cj/giphy.gif'),
+  'cloudy': await fetch('https://media.giphy.com/media/VJ5O80tOcyB3LYBIK2/giphy.gif'),
+  'rainy': await fetch('https://media.giphy.com/media/Twh6gTAd73n0uoMT5p/giphy.gif'),
+  'windy': await fetch('https://media.giphy.com/media/fYTUarigdyZyvwFQsU/giphy.gif'),
+  'snowy': await fetch('https://media.giphy.com/media/ggbeoGBT4t4WCuiq7S/giphy.gif'),
+  'stormy': await fetch('https://media.giphy.com/media/l1J9w4UPRtjZtPsA0/giphy.gif')
+}
+
+async function getWeather(location = 'vancouver'){
   const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=4db0f8caba1f4ca3991200732232512&q='${location}`);
   weatherImg.alt = location;
   const weatherData = await response.json();
   const weatherCondition = weatherData.current.condition.text;
+  weatherLabel.textContent = weatherCondition;
   weatherImg.alt = `${weatherCondition} image`;
-  console.log(weatherCondition);
+  locationLabel.textContent = weatherData.location.name;
   switch(weatherCondition){
     case 'Clear':
-      weatherImg.src = '';
+      weatherImg.src = weatherImages.sunny.url;
       break;
     case 'Overcast':
-      weatherImg.src = '';
+    case 'Partly Cloudy':
+    case 'Cloudy':
+      weatherImg.src = weatherImages.cloudy.url;
       break;
     case 'Rain':
-      weatherImg.src = '';
+    case 'Light rain':
+      weatherImg.src = weatherImages.rainy.url;
       break;
     case 'Snow':
-      weatherImg.src = '';
+      weatherImg.src = weatherImages.snowy.url;
       break;
     case 'Storm':
-      weatherImg.src = '';
+      weatherImg.src = weatherImages.stormy.url;
       break;
     default:
       weatherImg.src = weatherData.current.condition.icon;
