c3f4094b8ed60c26681318854785b08f5dcefedd
Iwan Rahman <1w3n.rahman@gmail.com>
2023-12-27 14:28:06 -0500

    Display image based on weather condition

--- src/index.js
+++ src/index.js
@@ -23,6 +23,10 @@ const cloudData = document.querySelector(".weather-data.vis > h5 + h5");
 const presData = document.querySelector(".weather-data.oth > h5");
 const uvData = document.querySelector(".weather-data.oth > h5 + h5");
 
+const country = document.querySelector("#country");
+const city = document.querySelector("#city");
+const btnSearch = document.querySelector('input[type="submit"]');
+
 const weatherImages = {
   'sunny': await fetch('https://media.giphy.com/media/UnyblOs6hGx9Mli7jq/giphy.gif'),
   'night': await fetch('https://media.giphy.com/media/MZXCIvPneFNeFF18Cj/giphy.gif'),
@@ -33,10 +37,15 @@ const weatherImages = {
   'stormy': await fetch('https://media.giphy.com/media/l1J9w4UPRtjZtPsA0/giphy.gif')
 }
 
-async function getWeather(location = 'vancouver'){
-  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=4db0f8caba1f4ca3991200732232512&q='${location}`);
-  weatherImg.alt = location;
-  const weatherData = await response.json();
+const weatherCodes = {
+  'clear': [1000],
+  'cloudy': [1003,1006,1009,1030,1135],
+  'rainy': [1063,1150,1153,1180,1183,1186,1189,1192,1195,1240,1243,1246],
+  'snowy': [1066,1069,1072,1114,1117,11147,1168,1171,1198,1201,1204,1207,1210,1213,1216,1219,1222,1225,1237,1249,1252,1255,1258,1261,1264],
+  'stormy': [1087,1273,1276,1279,1282]
+}
+
+function updateDisplay(weatherData){
   const weatherCondition = weatherData.current.condition.code;
 
   weatherLabel.textContent = weatherData.current.condition.text;
@@ -60,35 +69,45 @@ async function getWeather(location = 'vancouver'){
   presData.textContent = `Pres(mb): ${weatherData.current.pressure_mb}`;
   uvData.textContent = `UV: ${weatherData.current.uv}`;
   
-  switch(weatherCondition){
-    case 1000:
-      weatherImg.src = weatherImages.sunny.url;
-      break;
-    case weatherCondition >= 1003 && weatherCondition <= 1030:
-    case weatherCondition === 1135:
-      weatherImg.src = weatherImages.cloudy.url;
-      break;
-    case weatherCondition === 1063:
-    case weatherCondition >= 1150 && weatherCondition <= 1153:
-    case weatherCondition >= 1180 && weatherCondition <= 1195:
-    case weatherCondition >= 1240 && weatherCondition <= 1246:
-      weatherImg.src = weatherImages.rainy.url;
-      break;
-    case weatherCondition >= 1066 && weatherCondition <= 1072:
-    case weatherCondition >=1114 && weatherCondition <= 1117:
-    case weatherCondition === 1147:
-    case weatherCondition >= 1168 && weatherCondition <= 1171:
-    case weatherCondition >= 1198 && weatherCondition <= 1237:
-    case weatherCondition >= 1249 && weatherCondition <= 1264:
-      weatherImg.src = weatherImages.snowy.url;
-      break;
-    case weatherCondition === 1087:
-    case weatherCondition >= 1273 && weatherCondition <= 1282:
-      weatherImg.src = weatherImages.stormy.url;
-      break;
-    default:
-      weatherImg.src = weatherData.current.condition.icon;
-  }
+  const weather = (() => {
+    const weatherEntries = Object.entries(weatherCodes);
+    let weatherKey;
+
+    weatherEntries.find((element) => {
+      if(element[1].indexOf(weatherCondition) !== -1){
+        [weatherKey] = element
+        return true;
+      }
+      return false;
+    })
+    
+    return weatherKey;
+    })();
+
+  weatherImg.src = weatherImages[weather].url;
+}
+
+
+
+async function getWeather(location = 'vancouver'){
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=4db0f8caba1f4ca3991200732232512&q='${location}`);
+  weatherImg.alt = location;
+  const weatherData = await response.json();
+  updateDisplay(weatherData);
+
 }
 
 getWeather();
+
+const getSearch = (e) => {
+  const countryQuery = country.value;
+  const cityQuery = city.value; 
+  if(countryQuery !== ''){
+    getWeather(`${countryQuery}/${cityQuery}`);
+  }else{
+    getWeather(cityQuery);
+  }
+  e.preventDefault();
+}
+
+btnSearch.addEventListener('click',getSearch);
